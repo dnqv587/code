224f0fd89c5107523470cd331441c479ffe13b44
dnqv587 <1336047549@qq.com>
2022-01-11 13:19:18 +0800

    algorithms

---
+++ algorithms/dataStructure/sqQueue.hpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class DsqQueue
+{
+public:
+	DsqQueue()
+	{
+
+	}
+
+	~DsqQueue()
+	{
+
+	}
+
+private:
+	 
+
+private:
+
+	T* m_queue;
+
+	size_t m_size;
+};
