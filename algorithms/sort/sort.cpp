2d39d036cef6a498664927a52f47a5f334a27cdd
dnqv587 <1336047549@qq.com>
2022-03-03 12:18:03 +0800

    algorithms

--- algorithms/sort/sort.cpp
+++ algorithms/sort/sort.cpp
@@ -12,17 +12,47 @@ void myPut(int s[],int len )
 	cout << endl;
 }
 
-//√∞≈›≈≈–Ú
-void  bubble(int s[],int len)
+
+/*
+—°‘Ò≈≈–Ú
+ ±º‰∏¥‘”∂»£∫n^2
+ø’º‰∏¥‘”∂»£∫1
+*/
+void select(int* s, int len)
 {
-	
-	for (int i = 0; i < len; i++)
+	for (int i = 0; i < len-1; ++i)
 	{
-		for (int j = 0; j < len - i; j++)
+		int min = i;
+		for (int j = i+1; j < len; ++j)
 		{
-			if (s[j + 1] > s[j])
+			if (s[min] > s[j])
 			{
-				int temp = s[j];
+				min = j;
+			}
+		}
+		int temp;
+		temp = s[min];
+		s[min] = s[i];
+		s[i] = temp;
+	}
+}
+
+
+/*
+√∞≈›≈≈–Ú
+ ±º‰∏¥‘”∂»£∫n^2
+ø’º‰∏¥‘”∂»£∫1
+*/
+void bubble(int* s, int len)
+{
+	for (int i = 0; i < len; ++i)
+	{
+		for (int j = 0; j < len-i-1; ++j)
+		{
+			if (s[j] > s[j + 1])
+			{
+				int temp;
+				temp = s[j];
 				s[j] = s[j + 1];
 				s[j + 1] = temp;
 			}
@@ -30,50 +60,120 @@ void  bubble(int s[],int len)
 	}
 }
 
-//—°‘Ò≈≈–Ú
-void select(int s[],int len)
+/*
+≤Â»Î≈≈–Ú
+ ±º‰∏¥‘”∂»£∫n^2
+ø’º‰∏¥‘”∂»£∫1
+*/
+void insert(int* s, int len)
 {
-	for (int i = 0; i < len; i++)
+	for (int i = 1; i < len; ++i)
 	{
-		int min = i;
-		for (int j = i + 1; j < len; j++)
+		for (int j = i; j >0; --j)
 		{
-			if (s[min] > s[j])
+			if (s[j] < s[j - 1])
 			{
-				min = j;
+				int temp;
+				temp = s[j];
+				s[j] = s[j - 1];
+				s[j - 1] = temp;
 			}
-		}
-		int temp = s[i];
-		s[i] = s[min];
-		s[min]=temp;
-	}
-}
-
-//≤Â»Î≈≈–Ú
-void insert(int s[], int len)
-{
-	for (int i = 1; i < len; i++)
-	{
-		if (s[i - 1] > s[i])
-		{
-			int temp = s[i];
-			int j = i-1;
-			for (; j >= 0 && s[j - 1] > temp; j--)
-			{
-				s[j + 1] = s[j];
-			}
-			s[j+1] = temp;
 		}
 	}
 }
 
+/*
+œ£∂˚≈≈–Ú
+ ±º‰∏¥‘”∂»£∫n^1.3
+ø’º‰∏¥‘”∂»£∫1
+*/
+void shell(int* s, int len)
+{
+	for (int gap = len/2; gap > 0; gap /= 2)
+	{
+		for (int i = gap; i < len; i++)
+		{
+			for (int j = i; j > gap - 1; j -= gap)
+			{
+				if (s[j] < s[j - gap])
+				{
+					int temp = s[j];
+					s[j] = s[j - gap];
+					s[j - gap] = temp;
+				}
+			}
+		}
+	}
+
+
+}
+
+/*
+πÈ≤¢≈≈–Ú
+ ±º‰∏¥‘”∂»£∫nlogn
+ø’º‰∏¥‘”∂»£∫n
+*/
+void _merge(int* s, int begin, int mid, int end)
+{
+	int* temp = new int[end - begin + 1];
+	int pos = 0;
+	int left = begin;
+	int right = mid + 1;
+	while (left <= mid && right <= end)
+	{
+		if (s[left] > s[right])
+		{
+			temp[pos] = s[right];
+			++pos;
+			++right;
+		}
+		else
+		{
+			temp[pos] = s[left];
+			++pos;
+			++left;
+		}
+	}
+	while (left <= mid)
+	{
+		temp[pos] = s[left];
+		++pos;
+		++left;
+	}
+	while (right <= end)
+	{
+		temp[pos] = s[right];
+		++pos;
+		++right;
+	}
+	for (int i = 0; i < pos; ++i)
+	{
+		s[begin + i] = temp[i];
+	}
+
+	delete[] temp;
+}
+void merge(int* s, int begin,int end)
+{
+	if (begin>end)
+	{
+		return;
+	}
+	int mid = begin + (end - begin) / 2;
+	merge(s, begin, mid);//◊Û±ﬂ
+	merge(s, mid + 1, end);//”“±ﬂ
+	_merge(s, begin, mid, end);//∫œ≤¢
+
+}
 
 int main()
 {
-	int s[] = {5,64,19,654,216,848,3,5494,16,45,265,7896,164,134,111,123,32 }, len = sizeof(s)/sizeof(s[0]);
+	int s[] = {2,4,6,8,10,1,3,5,7,9,0 }, len = sizeof(s)/sizeof(s[0]);
 	//bubble(s,len);
 	//select(s, len);
-	insert(s, len);
+	//insert(s, len);
+	//shell(s, len);
+	merge(s,0, len);
 	myPut(s,len);
 	system("pause");
 	return 0;
