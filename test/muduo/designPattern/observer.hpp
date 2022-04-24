141c5430df9d7ea49b5a538b3157616d3ee3c41e
dnqv587 <1336047549@qq.com>
2022-04-24 17:17:12 +0800

    muduo

--- test/muduo/designPattern/observer.hpp
+++ test/muduo/designPattern/observer.hpp
@@ -1,20 +1,23 @@
 #pragma once
 #include <vector>
 #include <algorithm>
+#include<memory>
+#include "base/mutex.h"
 /*
 观察者模式
 */
 
 class Observable;
 
-class Observer
+class Observer :std::enable_shared_from_this<Observer>
 {
 public:
+
 	virtual ~Observer();
 
 	//纯虚函数，用于展示数据
 	virtual void update() = 0;
-	//被注册观察
+	//注册观察
 	void observe(Observable* observalbe);
 	
 
@@ -26,22 +29,11 @@ class Observable
 {
 public:
 	//通知所有观察者
-	void notify()
-	{
-		for (auto iter : m_Oberservers)
-		{
-			if (iter)
-			{
-				iter->update();
-			}
-		}
-	}
+	void notify();
 	//注册
-	void register_(Observer* oberserver)
-	{
-		m_Oberservers.push_back(oberserver);
-	}
-	//销毁
+	void register_(std::weak_ptr<Observer> oberserver);
+	/*
+	//注销
 	void unregister(Observer* observer)
 	{
 		auto iter = std::find(m_Oberservers.begin(), m_Oberservers.end(), observer);
@@ -51,19 +43,44 @@ public:
 			m_Oberservers.pop_back();
 		}
 	}
-
+	*/
 private:
-	std::vector<Observer*> m_Oberservers;
+	std::vector<std::weak_ptr<Observer>> m_Oberservers;
+	mutable MutexLock m_mutex;
 };
 
 
 Observer::~Observer()
 {
-	m_Obserberable->unregister(this);
+	//m_Obserberable->unregister(this);
 }
 
 void Observer::observe(Observable* observalbe)
 {
-	observalbe->register_(this);
+	observalbe->register_(shared_from_this());
 	m_Obserberable = observalbe;
+}
+
+void Observable::notify()
+{
+	MutexLockGuard lock(m_mutex);//加锁
+	auto iter = m_Oberservers.begin();
+	while (iter != m_Oberservers.end())
+	{
+		std::shared_ptr<Observer> obj(iter->lock());//尝试将weak_ptr提升为shared_ptr
+		if (obj)
+		{
+			obj->update();//此为线程安全的，因为obj存在于栈中
+			++iter;
+		}
+		else
+		{
+			iter = m_Oberservers.erase(iter);//对象已被销毁，从容器中删除
+		}
+	}
+}
+
+void Observable::register_(std::weak_ptr<Observer> oberserver)
+{
+	m_Oberservers.push_back(oberserver);
 }
--- test/muduo/main.cpp
+++ test/muduo/main.cpp
@@ -7,24 +7,28 @@ using namespace std;
 class Foo : public Observer
 {
 public:
-	~Foo()
-	{
-		m_Obserberable->unregister(this);
-	}
 
-	virtual void update()
+	virtual void update() override
 	{
 		printf("Foo::update() %p\n", this);
 	}
-	
 };
+
+void observerTest()
+{
+
+	Observable subject;
+	{
+		std::shared_ptr<Foo> p(new Foo);
+		p->observe(&subject);
+		subject.notify();
+	}
+	subject.notify();
+
+}
 
 int main(int argc, char* argv[])
 {
-	Observable o;
-	Foo* foo = new Foo;
-	foo->observe(&o);
-	o.notify();
-	delete foo;
-	o.notify();
+	observerTest();
+	return 0;
 }
