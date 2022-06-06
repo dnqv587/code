def186e28d7df92cfdd50758eed701b5975c2738
dnqv587 <1336047549@qq.com>
2022-06-07 00:26:04 +0800

    异步日志

--- test/muduo_linux/logger/AsynLogging.cc
+++ test/muduo_linux/logger/AsynLogging.cc
@@ -1,1 +1,123 @@
 #include "AsynLogging.h"
+#include "assert.h"
+#include "LogFile.h"
+#include "../time/Timestamp.h"
+
+AsynLogging::AsynLogging(std::string& baseName, off_t rollSize, int flushInterval /*= 3*/)
+	:m_baseName(baseName),m_rollSize(rollSize),m_flushInterval(flushInterval), m_running(false),
+	m_thread(std::bind(threadFunc,this),"logging"), m_mutex(),m_cond(m_mutex), m_latch(1),
+	currentBuffer(new Buffer), nextBuffer(new Buffer),m_buffers()
+{
+	currentBuffer->bzero();
+	nextBuffer->bzero();
+	m_buffers.reserve(16);//预留空间
+}
+
+AsynLogging::~AsynLogging()
+{
+	if (m_running)
+	{
+		this->stop();
+	}
+}
+
+void AsynLogging::append(const char* logLine, int len)
+{
+	MutexLockGuard lock(m_mutex);
+	if (currentBuffer->avail() > len)//剩余缓冲区可以容纳
+	{
+		currentBuffer->append(logLine, len);
+	}
+	else
+	{
+		m_buffers.push_back(std::move(currentBuffer));//将已填满的缓冲区push到m_buffers中
+		if (nextBuffer)
+		{
+			currentBuffer = std::move(nextBuffer);//将预备缓冲区移动为当前缓冲区
+		}
+		else
+		{
+			currentBuffer.reset(new Buffer);//new一个新的缓冲区为当前缓冲区
+		}
+		currentBuffer->append(logLine, len);
+		m_cond.notify();//通知异步日志线程
+	}
+}
+
+void AsynLogging::threadFunc()
+{
+	assert(m_running == true);
+	m_latch.countDown();
+	LogFile output(m_baseName, m_rollSize, false);
+	//预备缓冲区
+	BufferPtr newBuffer1(new Buffer);
+	BufferPtr newBuffer2(new Buffer);
+	newBuffer1->bzero();
+	newBuffer2->bzero();
+	BufferVector buffersToWrite;//待写入缓冲区容器
+	buffersToWrite.reserve(16);
+	
+	while (m_running)
+	{
+		assert(newBuffer1 && newBuffer1->lenght() == 0);
+		assert(newBuffer2 && newBuffer2->lenght() == 0);
+		assert(buffersToWrite.empty());
+		//临界区
+		{
+			MutexLockGuard lock(m_mutex);
+			if (m_buffers.empty())//是否有已满待写入缓冲
+			{
+				m_cond.waitTime(m_flushInterval);//无论是否有已满待写入缓冲，都在等待m_flushInterval时间后都将缓冲区刷入文件
+			}
+			m_buffers.push_back(std::move(currentBuffer));
+			currentBuffer = std::move(newBuffer1);
+			std::swap(buffersToWrite, m_buffers);
+			if (!nextBuffer)
+			{
+				newBuffer1 = std::move(newBuffer2);
+			}
+		}
+		assert(!buffersToWrite.empty());
+
+		if (buffersToWrite.size() > 25)//日志堆积，即生产者生产速度大于消费者消费速度，将丢弃日志
+		{
+			char buf[256];
+			snprintf(buf, sizeof buf, "Dropped log messages at %s, %zd larger buffers\n",
+				Timestamp::now().formatString().c_str(),
+				buffersToWrite.size() - 2);
+			//fputs(buf, stderr);
+			::fwrite(buf, 1, sizeof(buf), stderr);
+			output.append(buf, static_cast<int>(strlen(buf)));
+		}
+		//写入日志文件
+		for (const auto& buffer : buffersToWrite)
+		{
+			output.append(buffer->data(), buffer->lenght());
+		}
+
+		if (buffersToWrite.size() > 2)
+		{
+			// drop non-bzero-ed buffers, avoid trashing
+			buffersToWrite.resize(2);
+		}
+		//将已写入的缓冲区归还
+		if (!newBuffer1)
+		{
+			assert(!buffersToWrite.empty());
+			newBuffer1 = std::move(buffersToWrite.back());
+			buffersToWrite.pop_back();
+			newBuffer1->reset();
+		}
+		if (!newBuffer2)
+		{
+			assert(!buffersToWrite.empty());
+			newBuffer2 = std::move(buffersToWrite.back());
+			buffersToWrite.pop_back();
+			newBuffer2->reset();
+		}
+		buffersToWrite.clear();
+		output.flush();	
+	}
+	output.flush();
+}
+
--- test/muduo_linux/logger/AsynLogging.h
+++ test/muduo_linux/logger/AsynLogging.h
@@ -38,14 +38,20 @@ private:
 	void threadFunc();//异步日志线程
 
 	typedef Buffer<LARGE_BUFFERSIZE> Buffer;//4MB
-	typedef std::vector<std::unique_ptr<Buffer>> BufferPtr;
+	typedef std::vector<std::unique_ptr<Buffer>> BufferVector;
+	typedef BufferVector::value_type BufferPtr;
 
 	std::atomic<bool> m_running;//线程运行标识
 	Thread m_thread;//线程
+	MutexLock m_mutex;
 	Condition m_cond;
 	CountDownLatch m_latch;
 	const std::string m_baseName;
-	const off_t m_rollSize;
-	const int m_flushInterval;
+	const off_t m_rollSize;//文件刷新大小
+	const int m_flushInterval;//刷新间隔
+	//临界值
+	BufferPtr currentBuffer;//双缓冲--当前缓冲
+	BufferPtr nextBuffer;//预备缓冲
+	BufferVector m_buffers;//待写入文件的已填满的缓冲
 };
 
