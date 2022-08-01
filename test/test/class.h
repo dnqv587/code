41e17a867544eac86e5e452f93477cdfe096c85f
dnqv587 <1336047549@qq.com>
2022-08-01 19:30:40 +0800

    test

--- test/test/class.h
+++ test/test/class.h
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <string>
 #include <utility>
+#include <exception>
+#include <stdlib.h>
+#include <assert.h>
+#include <string>
+
+#include <fstream>
 
 //virtual析构测试
 class Vbase
@@ -87,7 +93,7 @@ public:
 	//{
 	//	if (this == &that)//避免自我复制
 	//	{
-	//		return;
+	//		return *this;
 	//	}
 	//	delete this->p;
 	//	this->p = new int(*that.p);//深拷贝
@@ -119,4 +125,197 @@ public:
 	}
 
 	int* p;
+};
+
+//异常处理
+class MyException :public std::exception
+{
+public:
+	MyException()
+		:std::exception("this is MyException", 1)
+	{
+
+	}
+
+};
+
+//隐式类型转换
+class Person
+{
+public:
+	Person() = default;
+	Person(const char* name, int age)
+		:_name(name),
+		_age(age)
+	{
+
+	}
+	Person(const Person& that)
+		:_age(that._age),
+		_name(that._name)
+	{
+		printf("Person copy constructor\n");
+	}
+
+	const char* _name;
+	int _age;
+};
+
+class PersonWrapper
+{
+public:
+	PersonWrapper() = default;
+
+	PersonWrapper(Person& person)
+		:_person(person)
+	{
+
+	}
+
+	PersonWrapper(const PersonWrapper& that)
+	{
+		this->_person = that._person;
+		printf("PersonWrapper copy constructor\n");
+	}
+
+	operator Person()
+	{
+		return _person;
+
+	}
+private:
+	Person _person;
+};
+
+
+//18-19 接口设计
+struct Year
+{
+	Year() = default;
+	explicit Year(int val)
+		:Y(val)
+	{
+		assert(Y > 1970 && Y < 2100);
+	}
+	Year(const Year& y)
+		:Y(y.Y)
+	{
+		assert(Y > 1970 && Y < 2100);
+	}
+
+	operator int()
+	{
+		return Y;
+	}
+
+	int Y;
+};
+
+struct Month
+{
+	Month() = default;
+	explicit Month(int val)
+		:M(val)
+	{
+		assert(M > 0 && M < 13);
+	}
+	Month(const Month& m)
+		:M(m.M)
+	{
+		assert(M > 0 && M < 13);
+	}
+
+	operator int()
+	{
+		return M;
+	}
+
+	int M;
+};
+
+struct Day
+{
+	Day() = default;
+	explicit Day(int val)
+		:D(val)
+	{
+		if (D == 1 || D == 3 || D == 5 || D == 7 || D == 8 || D == 10 || D == 12)
+		{
+			assert(D > 0 && D <= 31);
+		}
+		else
+		{
+			if (D == 2)
+			{
+				assert(D > 0 && D <= 29);
+			}
+			assert(D > 0 && D <= 30);
+		}
+	}
+
+	Day(const Day& d)
+		:D(d.D)
+	{
+		if (D == 1 || D == 3 || D == 5 || D == 7 || D == 8 || D == 10 || D == 12)
+		{
+			assert(D > 0 && D <= 31);
+		}
+		else
+		{
+			assert(D > 0 && D <= 30);
+		}
+
+	}
+
+	operator int()
+	{
+		return D;
+	}
+
+	int D;
+};
+
+class Date
+{
+public:
+	explicit Date(Year Y, Month M, Day D)
+		:_Y(Y),
+		_M(M),
+		_D(D)
+	{
+
+	}
+
+	Date(int y, int m, int d)
+		:_Y(Year (y)),
+		_M(Month (m)),
+		_D(Day (d))
+	{
+
+	}
+
+	std::string format()
+	{
+		return "Year:" + std::to_string(_Y.Y) + " Month:" + std::to_string(_M.M) + " Day:" + std::to_string(_D.D);
+	}
+
+	Year year()
+	{
+		return _Y;
+	}
+
+	Month month()
+	{
+		return _M;
+	}
+
+	Day day()
+	{
+		return _D;
+	}
+
+private:
+	Year _Y;
+	Month _M;
+	Day _D;
 };
--- test/test/main.cc
+++ test/test/main.cc
@@ -1,6 +1,14 @@
 #include <iostream>
 #include "class.h"
 
+#include <string>
+#include <iterator>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <stdlib.h>
+#include <vector>
 
 void test1()
 {
@@ -29,10 +37,65 @@ void test3()
 	
 }
 
+void exp()
+{
+	try
+	{
+		throw MyException();
+		//throw std::bad_alloc();
+	}
+	catch (std::exception& e)
+	{
+		throw e;
+	}
+	catch (MyException& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
+
+void test4()
+{
+	try
+	{
+		//throw MyException();
+		//throw std::bad_alloc();
+		exp();
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	catch (MyException& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
+
+void test5()
+{
+	Person p("lili", 12);
+	PersonWrapper pw(p);
+	
+	Person p2 = pw;
+	std::cout << p2._name << p2._age << std::endl;
+}
+
+void test6()
+{
+	Date date(2022, 3, 23);
+	std::cout << date.format() << std::endl;
+	std::cout << date.year() << date.month() << date.day() << std::endl;
+}
+
 int main(int argc, char* argv[])
 {
 	//test1();
 	//test2();
-	test3();
+	//test3();
+	//test4();
+	//test5();
+	test6();
 	return 0;
 }
+
