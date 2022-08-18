3b6896241aef07f02c0630722bee18ea01f584a8
dnqv587 <1336047549@qq.com>
2022-08-18 23:20:13 +0800

    muduo

--- test/muduo_linux/time/TimerQueue.cc
+++ test/muduo_linux/time/TimerQueue.cc
@@ -10,9 +10,9 @@ std::vector<TimerQueue::Entry> TimerQueue::getExpired(Timestamp now)
 	Entry sentry = std::make_pair(now, std::unique_ptr<Timer>(reinterpret_cast<Timer*>(UINTPTR_MAX)));
 	TimerList::iterator iter = m_timers.lower_bound(sentry);//查找第一个不小于sentry的迭代器
 	assert(iter != m_timers.end() || now < iter->first);
-	//std::copy(m_timers.begin(), iter, std::back_inserter(expired));
-	//m_timers.erase(m_timers.begin(), iter);
-	std::move(m_timers.begin(), iter, std::back_inserter(expired));
+	std::copy(m_timers.begin(), iter, std::back_inserter(expired));
+	m_timers.erase(m_timers.begin(), iter);
+	//std::move(m_timers.begin(), iter, std::back_inserter(expired));
 
 	return expired;
 }
