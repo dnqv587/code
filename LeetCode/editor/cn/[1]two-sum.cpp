75128a18f9b33480da6f07c06e4aec2e135dabf4
dnqv587 <1336047549@qq.com>
2023-05-05 09:58:38 +0800

    LeetCode

---
+++ LeetCode/editor/cn/[1]two-sum.cpp
@@ -0,0 +1,79 @@
+1、两数之和
+/**
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。 
+
+ 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。 
+
+ 你可以按任意顺序返回答案。 
+
+ 
+
+ 示例 1： 
+
+ 
+输入：nums = [2,7,11,15], target = 9
+输出：[0,1]
+解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ 
+
+ 示例 2： 
+
+ 
+输入：nums = [3,2,4], target = 6
+输出：[1,2]
+ 
+
+ 示例 3： 
+
+ 
+输入：nums = [3,3], target = 6
+输出：[0,1]
+ 
+
+ 
+
+ 提示： 
+
+ 
+ 2 <= nums.length <= 10⁴ 
+ -10⁹ <= nums[i] <= 10⁹ 
+ -10⁹ <= target <= 10⁹ 
+ 只会存在一个有效答案 
+ 
+
+ 
+
+ 进阶：你可以想出一个时间复杂度小于 O(n²) 的算法吗？ 
+
+ Related Topics 数组 哈希表 👍 16695 👎 0
+
+*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+		size_t count = nums.size();
+		std::vector<int> ret;
+		std::set<int> unique;
+		for (int i = 0; i < count;++i)
+		{
+			for (int j = i+1; j < count;++j)
+			{
+				if(nums[i]+nums[j]==target)
+				{
+					unique.insert(i);
+					unique.insert(j);
+				}
+
+			}
+		}
+		for(auto i:unique)
+		{
+			ret.push_back(i);
+		}
+
+		return ret;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
