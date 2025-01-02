b746068d0411343ac8e437d8a0f4800a41131066
dainq45972 <1336047549@qq.com>
2025-01-02 18:18:15 +0800

    learning asio

--- asio/lessons/lessons1.cc
+++ asio/lessons/lessons1.cc
@@ -1,31 +1,76 @@
 #include <iostream>
-#include <system_error>
-#include "asio/ip/address.hpp"
 #include "asio/ip/tcp.hpp"
-#include "asio/io_context.hpp"
+#include "asio/io_service.hpp"
+#include <memory>
+#include <thread>
+#include <future>
 
-void server()
+//***********************************************
+// name      : 同步echo示例
+// brief     :
+// attention : None
+//***********************************************
+
+using namespace asio;
+
+void service()
 {
-    asio::io_context io;
-    asio::ip::tcp::acceptor acc(io, asio::ip::tcp::endpoint{asio::ip::address_v6::any(),8088});
+	io_service service;
+	ip::tcp::endpoint ep(ip::tcp::v4(),2001);
+	ip::tcp::acceptor acc(service,ep);
+	while(true)
+	{
+		std::shared_ptr<ip::tcp::socket> sock = std::make_shared<ip::tcp::socket>(service);
+		acc.accept(*sock);
+		std::thread th([sock](){
+			while(true)
+			{
+				char data[64];
+				memset(data,0x00,sizeof(data));
+				size_t len = sock->read_some(buffer(data));
+				if (len > 0)
+				{
+					sock->write_some(buffer(data));
+				}
+			}
+		});
+		th.detach();
+	}
 }
+
 void client()
 {
-    std::error_code ec;
-    asio::ip::address addr = asio::ip::address::from_string("127.0.0.1",ec);
-    if(ec)
-    {
-        std::cout << ec.message() << std::endl;
-    }
-    asio::ip::tcp::endpoint ep(addr, 8088);
-
-    asio::io_context io;
-    asio::ip::tcp::socket sock(io);
-
-
+	io_service service;
+	ip::tcp::endpoint ep(ip::address::from_string("127.0.0.1"),2001);
+	ip::tcp::socket  sock(service);
+	sock.connect(ep);
+	char buf [1024];
+	while(true)
+	{
+		::memset(buf,0x00,sizeof(buf));
+		::read(STDIN_FILENO,buf,sizeof(buf));
+		sock.write_some(buffer(buf));
+		::memset(buf,0x00,sizeof(buf));
+		sock.read_some(buffer(buf));
+		std::cout<<"read="<<buf<<std::endl;
+	}
 
 }
+
 int main(int argc, char const *argv[])
 {
+	if(argc > 1 && ::strcmp(argv[1], "c") == 0)
+	{
+		std::cout<<"client"<<std::endl;
+		auto c = std::async(client);
+		c.wait();
+	}
+	else
+	{
+		std::cout<<"server"<<std::endl;
+		auto s = std::async(service);
+		s.wait();
+	}
 
+	return 0;
 }
---
+++ asio/lessons/lessons2.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+#include "asio/ip/tcp.hpp"
+#include "asio/io_service.hpp"
+#include <memory>
+#include <thread>
+#include <future>
+#include <system_error>
+#include <functional>
+//***********************************************
+// name      : 异步echo示例
+// brief     : 
+// attention : None
+//***********************************************
+using namespace asio;
+
+void service()
+{
+	io_service service;
+	ip::tcp::endpoint ep(ip::tcp::v4(),2001);
+	ip::tcp::acceptor acc(service,ep);
+
+	std::shared_ptr<ip::tcp::socket> sock = std::make_shared<ip::tcp::socket>(service);
+
+	acc.async_accept(*sock,[sock,&acc,&service](std::error_code ec){
+	  if(ec)
+		  return;
+	  while(true)
+	  {
+		  char data[64];
+		  memset(data,0x00,sizeof(data));
+		  size_t len = sock->read_some(buffer(data));
+		  if (len > 0)
+		  {
+			  sock->write_some(buffer(data));
+		  }
+	  }
+	});
+	service.run();
+
+}
+
+void client()
+{
+	io_service service;
+	ip::tcp::endpoint ep(ip::address::from_string("127.0.0.1"),2001);
+	ip::tcp::socket  sock(service);
+	char buf [1024];
+	sock.async_connect(ep,[&buf,&sock](std::error_code code){
+		if(code)
+			return;
+	  while(true)
+	  {
+		  ::memset(buf, 0x00, sizeof(buf));
+		  ::read(STDIN_FILENO, buf, sizeof(buf));
+		  sock.write_some(buffer(buf));
+		  ::memset(buf, 0x00, sizeof(buf));
+		  sock.read_some(buffer(buf));
+		  std::cout << "read=" << buf << std::endl;
+	  }
+	});
+
+	service.run();
+}
+
+int main(int argc, char const *argv[])
+{
+	if(argc > 1 && ::strcmp(argv[1], "c") == 0)
+	{
+		std::cout<<"client"<<std::endl;
+		auto c = std::async(client);
+		c.wait();
+	}
+	else
+	{
+		std::cout<<"server"<<std::endl;
+		auto s = std::async(service);
+		s.wait();
+	}
+}
