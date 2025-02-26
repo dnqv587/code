37742a96a265f10d784198bf862dad4014222065
dnqv587 <1336047549@qq.com>
2025-02-26 23:40:50 +0800

    mock

--- mock/src/lesson1.cc
+++ mock/src/lesson1.cc
@@ -19,11 +19,11 @@ TEST(FooTest,HandleNoneZeroInput)
    EXPECT_EQ(6,Foo(30,18));
 }
 
-int main(int argc,char*argv[])
-{
-    
-    testing::InitGoogleTest(&argc,argv);
-
-    return RUN_ALL_TESTS();
-}
+//int main(int argc,char*argv[])
+//{
+//
+//    testing::InitGoogleTest(&argc,argv);
+//
+//    return RUN_ALL_TESTS();
+//}
 
